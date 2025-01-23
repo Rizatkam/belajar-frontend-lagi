1186716c64e6635466f4d686c62a2403d4fa7a6b
Rizatkam <rizalsatriakamal@gmail.com>
2025-01-23 12:29:22 +0700

    Exercise 2

--- src/InputAge.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-const InputAge = (props) => {
-return <label>
-Input Age :
-<input type="number" value={props.age}
-onChange={props.changeAge}/>
-</label>;
-}
-export default InputAge;
--- src/InputName.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-const InputName = (props) => {
-return <label>
-Input Name :
-<input value={props.name} onChange={props.changeName}/>
-</label>;
-}
-export default InputName;
