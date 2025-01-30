cdb3e93a59964def8764b641b2b2ff5eb1b5b103
Rizatkam <rizalsatriakamal@gmail.com>
2025-01-30 17:05:28 +0700

    Masih Gagal

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
 import "./App.css";
 import Home from "./Components/Home";
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Home />
-      </div>
-    );
-  }
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <Home />
+//       </div>
+//     );
+//   }
+// }
+const App=()=>{
+  return(
+    <Home/>
+  )
 }
 
 export default App;
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,24 +1,43 @@
-import React, {useState} from 'react';
+// import React, {useState} from 'react';
 import Signup from '../Components/Signup';
-import Login from '../Components/Login';
-import Profile from '../Components/Profile';
+// import Login from '../Components/Login';
+// import Profile from '../Components/Profile';
 import {Tabs,Tab} from 'react-bootstrap';
 
-const Home = (props) => {
-    const [data,setData]=useState({})
+// const Home = (props) => {
+//     const [data,setData]=useState({})
+//     return(
+//         <Tabs defaultActiveKey="SignUp" id="uncontrolled-tab-example">
+// <Tab eventKey="SignUp" title="SignUp">
+// <Signup dataSignup={(data)=>setData(data)}/>
+// </Tab>
+// <Tab eventKey="Login" title="Login">
+// <Login data={data}/>
+// </Tab>
+// <Tab eventKey="Profile" title="Profile">
+// <Profile data={data}/>
+// </Tab>
+// </Tabs> 
+//       )
+// }
+
+const Home =(props)=>{
     return(
-        <Tabs defaultActiveKey="SignUp" id="uncontrolled-tab-example">
-<Tab eventKey="SignUp" title="SignUp">
-<Signup dataSignup={(data)=>setData(data)}/>
-</Tab>
-<Tab eventKey="Login" title="Login">
-<Login data={data}/>
-</Tab>
-<Tab eventKey="Profile" title="Profile">
-<Profile data={data}/>
-</Tab>
-</Tabs> 
-      )
+        <Tabs
+        defaultActiveKey="SignUp"
+        id="uncontrolled-tab-example"
+      >
+        <Tab eventKey="SignUp" title="SignUp">
+          <Signup/>
+        </Tab>
+        <Tab eventKey="Login" title="Login">
+          Tab content for Login
+        </Tab>
+        <Tab eventKey="Profile" title="Profile">
+          Tab content for Profile
+        </Tab>
+      </Tabs>
+    )
 }
 
 export default Home;
